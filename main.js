5be04fafef85c722cf5f8d9c3f2c30ba690ddf71
aqua vedant <v.arora1@share.epsb.ca>
2025-06-03 13:01:29 -0600

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// Functionize Minecraft Fishing Start Code
-
-// Fish Button Event Listener
-document.getElementById("fish-btn").addEventListener("click", fishBtnClicked);
-
-function fishBtnClicked() {
-  console.log("Fish");
-}
